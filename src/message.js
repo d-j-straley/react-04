70ba71aec48a0ac89bac629ed32a04f1781b58ef
David J. Straley <djstraley@sandyhook.com>
2025-03-15 19:29:28 -0500

    this demos using a default export and then importing and using the same

--- src/message.js
+++ src/message.js
@@ -1,7 +1,12 @@
 const message = () => {
   const name = "Jesse";
-  const age = 40;
-  return name + ' is ' + age + 'years old.';
+  const age = 41;
+  return name + ' is ' + age + ' years old.';
 };
 
 export default message;
+
+// this is actually exporting a function that returns a string.The function is called message and it returns a string that says "Jesse is 40 years old.".
+// The function is exported as the default export, so when we import it, we can name it whatever we want.
+//     also, this is different than importing a named export, because in that
+// case we are really importing gapProperties, not a function.
