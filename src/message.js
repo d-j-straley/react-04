723d067f394cb0dbd7a2b9425ff6bd4b8a7b860c
David J. Straley <djstraley@sandyhook.com>
2025-03-16 21:05:49 -0500

    fixing comments

--- src/message.js
+++ src/message.js
@@ -6,7 +6,7 @@ const message = () => {
 
 export default message;
 
-// this is actually exporting a function that returns a string.The function is called message and it returns a string that says "Jesse is 40 years old.".
+// this is actually exporting a function that returns a string.The function is called message and it returns a string that says "Jesse is 41 years old.".
 // The function is exported as the default export, so when we import it, we can name it whatever we want.
 //     also, this is different than importing a named export, because in that
-// case we are really importing gapProperties, not a function.
+// case we are really importing Properties, not a function.
