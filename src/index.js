b730eb33fceee65100c695bc3b1ab88ca6e82c80
David J. Straley <djstraley@sandyhook.com>
2025-03-17 17:06:48 -0500

    this demonstrates how to quickly change the contents of the main web page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// const myFirstElement = <h1>Hello React!</h1>
+const myElement = <h1>Hello React!</h1>
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(myFirstElement);
-
-const myArray = ['apple', 'banana', 'cherry'];
-const myList = myArray.map((item) => <li>{item}</li>);
-const myElement = <ul>{myList}</ul>;
+// const myArray = ['apple', 'banana', 'cherry'];
+// const myList = myArray.map((item) => <li>{item}</li>);
+// const myElement = <ul>{myList}</ul>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
