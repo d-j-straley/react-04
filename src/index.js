06ed6e5109fedec8d5937dde7a8fd8d62cbbb5fb
David J. Straley <djstraley@sandyhook.com>
2025-03-16 20:08:01 -0500

    updated the array may and displayed the same

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello React!</h1>
+// const myFirstElement = <h1>Hello React!</h1>
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myFirstElement);
+
+const myArray = ['apple', 'banana', 'cherry'];
+const myList = myArray.map((item) => <li>{item}</li>);
+const myElement = <ul>{myList}</ul>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+
+root.render(myElement);
