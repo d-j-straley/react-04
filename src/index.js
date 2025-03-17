97241bda8fd4ef8ce6a32b6c4b9b623fad63644e
David J. Straley <djstraley@sandyhook.com>
2025-03-17 17:51:11 -0500

    create a table in main winndow

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myElement = <h1>Hello React!</h1>
+// const myElement = <h1>Hello React!</h1>
 
 // const myArray = ['apple', 'banana', 'cherry'];
 // const myList = myArray.map((item) => <li>{item}</li>);
 // const myElement = <ul>{myList}</ul>;
+
+
+const myElement = (
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <td>John</td>
+        </tr>
+        <tr>
+            <td>Elsa</td>
+        </tr>   
+    </table>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
