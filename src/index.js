04df0b78e14cbcabac7f0c0d7548e1279ef5198c
David J. Straley <djstraley@sandyhook.com>
2025-03-29 14:00:08 -0500

    demonstrates getSnapshotBeforeUpdate( )

--- src/index.js
+++ src/index.js
@@ -12,9 +12,21 @@ class Header extends React.Component {
       this.setState({favoritecolor: "yellow"})
     }, 1000)
   }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+    "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+    "The updated favorite is " + this.state.favoritecolor;
+  }
   render() {
     return (
+      <div>
       <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <div id="div1"></div>
+      <div id="div2"></div>
+      </div>
     );
   }
 }
