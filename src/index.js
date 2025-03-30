32df94b095fc973c5aaecb1f4781618336b96250
David J. Straley <djstraley@sandyhook.com>
2025-03-30 17:30:39 -0500

    demo of Textarea

--- src/index.js
+++ src/index.js
@@ -2,54 +2,20 @@ import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 function MyForm() {
-  const [inputs, setInputs] = useState({});
+  const [textarea, setTextarea] = useState(
+    "The content of a textarea goes in the value attribute"
+  );
 
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
-
-  /* Note that we use this one event handler for both fields.
-  Although we could have attached separate event handlers to each field,
-  this is a more efficient way to handle the change events,
-  and is also cleaner and preferred in React.
-  This is a common pattern in React, where we use a single event handler
-  to handle multiple inputs.
-  */
-   
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
+    setTextarea(event.target.value)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-      <input 
-        type="text" 
-        name="username" 
-        value={inputs.username || ""} 
-        onChange={handleChange}
-      />
-      </label>
-      <label>Enter your age:
-        <input 
-          type="number" 
-          name="age" 
-          value={inputs.age || ""} 
-          onChange={handleChange}
-        />
-        </label>
-        <input type="submit" />
+    <form>
+      <textarea value={textarea} onChange={handleChange} />
     </form>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyForm />);
-
-/*
-Click F12 and navigate to the "Console view"
-to see the result when you submit the form.
-*/
