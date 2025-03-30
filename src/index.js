62875317b521b5dee3206d864a73c440da7c8a97
David J. Straley <djstraley@sandyhook.com>
2025-03-30 16:56:30 -0500

    demo on using "useState" hook to manage input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-
-import React from 'react';
+import { useState } from "react";
 import ReactDOM from 'react-dom/client';
 
 function MyForm() {
+  const [name, setName] = useState("");
+
   return (
     <form>
       <label>Enter your name:
-        <input type="text" />
+        <input
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </label>
     </form>
   )
