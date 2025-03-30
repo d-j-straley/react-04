f08b5cf34ba77b87e5634eedcd89804a48c532d5
David J. Straley <djstraley@sandyhook.com>
2025-03-30 17:31:59 -0500

    demo of using a select list

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 function MyForm() {
-  const [textarea, setTextarea] = useState(
-    "The content of a textarea goes in the value attribute"
-  );
+  const [myCar, setMyCar] = useState("Volvo");
 
   const handleChange = (event) => {
-    setTextarea(event.target.value)
+    setMyCar(event.target.value)
   }
 
   return (
     <form>
-      <textarea value={textarea} onChange={handleChange} />
+      <select value={myCar} onChange={handleChange}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo">Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
     </form>
   )
 }
