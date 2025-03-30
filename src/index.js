59325810439e025bec8b87bf5a4775161511052d
David J. Straley <djstraley@sandyhook.com>
2025-03-30 17:09:27 -0500

    demo with two submit fields

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
 import { useState } from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
 function MyForm() {
-  const [name, setName] = useState("");
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`The name you entered was: ${name}`);
+    console.log(inputs);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label>Enter your name:
-        <input 
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <input 
+        type="text" 
+        name="username" 
+        value={inputs.username || ""} 
+        onChange={handleChange}
+      />
       </label>
-      <input type="submit" />
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
     </form>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<MyForm />);
+
+/*
+Click F12 and navigate to the "Console view"
+to see the result when you submit the form.
+*/
