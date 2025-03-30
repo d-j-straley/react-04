6ee54ac28fa2a0e78ec8519b4404b6612a93fc17
David J. Straley <djstraley@sandyhook.com>
2025-03-30 17:00:04 -0500

    demo using a "Submit" button and an event handler

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import ReactDOM from 'react-dom/client';
 function MyForm() {
   const [name, setName] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Enter your name:
-        <input
+        <input 
           type="text" 
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </label>
+      <input type="submit" />
     </form>
   )
 }
