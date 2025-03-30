3db6e556ceca1b56df08f4bd8d8b36b2ad0bf4a0
David J. Straley <djstraley@sandyhook.com>
2025-03-29 20:35:27 -0500

    simple form 01

--- src/index.js
+++ src/index.js
@@ -1,38 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class Container extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {show: true};
-  }
-  delHeader = () => {
-    this.setState({show: false});
-  }
-  render() {
-    let myheader;
-    if (this.state.show) {
-      myheader = <Child />;
-    };
-    return (
-      <div>
-      {myheader}
-      <button type="button" onClick={this.delHeader}>Delete Header</button>
-      </div>
-    );
-  }
-}
-
-class Child extends React.Component {
-  componentWillUnmount() {
-    alert("The component named Header is about to be unmounted.");
-  }
-  render() {
-    return (
-      <h1>Hello World!</h1>
-    );
-  }
+function MyForm() {
+  return (
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Container />);
+root.render(<MyForm />);
