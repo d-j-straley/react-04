7075a36366c39e2dd16d61850b989dd1224692c6
David J. Straley <djstraley@sandyhook.com>
2025-03-31 13:46:42 -0500

    explanation of prop behavior added.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,20 @@ function Car(props) {
 }
 
 function Garage() {
+  const carName = "Toyota";
   return (
     <>
-	    <h1>Who lives in my garage?</h1>
-	    <Car brand="Ford" />
+      <h1>Who lives in my garage?</h1>
+      /*
+      Note that it is *here* that we define what the input parameter
+      of the Car component will be. The Car component will
+      receive the value of carName as a prop, and we can use
+      it in the Car component.
+      And in the Car component, we refer to that value via the "variable name"
+      which was defined in the *calling* component, which here is: "brand."
+      */
+      "
+	    <Car brand={ carName } />
     </>
   );
 }
