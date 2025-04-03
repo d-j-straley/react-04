25738cda24d862a19f9eb318caa5be1beedd9fd3
David J. Straley <djstraley@sandyhook.com>
2025-04-03 15:04:00 -0500

    using ternary operator

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Garage(props) {
-  const cars = props.cars;
-  return (
-    <>
-      <h1>Garage</h1>
-      {cars.length > 0 &&
-        <h2>
-          You have {cars.length} cars in your garage.
-        </h2>
-      }
-    </>
-  );
+function MissedGoal() {
+	return <h1>MISSED!</h1>;
 }
 
-const cars = ['Ford', 'BMW', 'Audi'];
+function MadeGoal() {
+	return <h1>GOAL!</h1>;
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+	return (
+		<>
+			{ isGoal ? <MadeGoal/> : <MissedGoal/> }
+		</>
+	);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Garage cars={cars} />);
+root.render(<Goal isGoal={false} />);
