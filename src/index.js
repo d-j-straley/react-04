8dd744582d0e3d23fe4fbde023f6dd34d7e1024f
David J. Straley <djstraley@sandyhook.com>
2025-04-03 14:33:15 -0500

    demo using conditional without actually having an "if"

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function MissedGoal() {
-	return <h1>MISSED!</h1>;
+function Garage(props) {
+  const cars = props.cars;
+  return (
+    <>
+      <h1>Garage</h1>
+      {cars.length > 0 &&
+        <h2>
+          You have {cars.length} cars in your garage.
+        </h2>
+      }
+    </>
+  );
 }
 
-function MadeGoal() {
-	return <h1>GOAL!</h1>;
-}
-
-function Goal(props) {
-  const isGoal = props.isGoal;
-  if (isGoal) {
-    return <MadeGoal/>;
-  }
-  return <MissedGoal/>;
-}
-
+const cars = ['Ford', 'BMW', 'Audi'];
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// Note taht if a value is passed in which does not resolve to either true or false, the component will not render anything.
-root.render(<Goal isGoal={true} />);
+root.render(<Garage cars={cars} />);
