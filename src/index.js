60a283e3abf2fdd746503241e212f6fd5001d725
David J. Straley <djstraley@sandyhook.com>
2025-04-04 16:33:40 -0500

    demo using map() but this time with associated keys

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ function Car(props) {
 }
 
 function Garage() {
-  const cars = ['Ford', 'BMW', 'Audi'];
+  const cars = [
+    {id: 1, brand: 'Toyota'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
   return (
     <>
 	    <h1>Who lives in my garage?</h1>
 	    <ul>
-        {cars.map((car) => <Car brand={car} />)}
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
       </ul>
     </>
   );
