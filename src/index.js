f8cec08c9172520738749e307f572b3dfbbb1497
David J. Straley <djstraley@sandyhook.com>
2025-04-06 14:39:52 -0500

    demo creating a separate style object in JavaScript so as to pass it into React

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const Header = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "50px",
+    fontFamily: "Sans-Serif",
+    textAlign: "center",
+    fontSize: "20px",
+  };
   return (
     <>
-      <h1 style={{backgroundColor: "lightblue"}}>Hello Style!</h1>
+      <h1 style={myStyle}>Hello Style!</h1>
       <p>Add a little style!</p>
     </>
   );
