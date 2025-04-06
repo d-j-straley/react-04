4be17e3248ced8894423994a5ae87728797acfbf
David J. Straley <djstraley@sandyhook.com>
2025-04-06 15:02:25 -0500

    demo using separate CSS "module" file

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,7 @@
+import styles from './style02.module.css';
+
 function Car()
 {
-    return <h2>This is a Car component.</h2>
+    return <h2 className={styles.green01}>This is a Car component.</h2>
 }
 export default Car;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles01.css';
-
-/*
- * note that when importing a separate CSS file,
- * that file is written in normal CSS syntax, using
- * normal CSS selectors, not CSS-in-JS syntax camelCase
- * styles.
- */
-
-const Header = () => {
-  return (
-    <>
-      <h1>Hello Style!</h1>
-      <p>Add a little style!.</p>
-    </>
-  );
-}
+import Car from './Car.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Header />);
+root.render(<Car />);
