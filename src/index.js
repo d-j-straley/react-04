ba7d0de71530886dac77af2b8719da4df4d637d2
David J. Straley <djstraley@sandyhook.com>
2025-04-07 14:43:20 -0500

    demo showing multiple state values

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
+
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-function FavoriteColor() {
-  const [color, setColor] = useState("red");
 
+function Car() {
+  const [brand, setBrand] = useState("Ford");
+  const [model, setModel] = useState("Mustang");
+  const [year, setYear] = useState("1964");
+  const [color, setColor] = useState("red");
+  
   return (
     <>
-      <h1>My favorite color is {color}!</h1>
-      <button
-        type="button"
-        onClick={() => setColor("blue")}
-          >Blue</button>
-          
-          <button type="button" onClick={() => setColor("green")}>
-            Green
-          </button>
-      <button type="button" onClick={() => setColor("red")}>
-        Red
-      </button>
-      <button type="button" onClick={() => setColor("yellow")}>
-        Yellow
-          </button>
-      </>
+      <h1>My {brand}</h1>
+      <p>
+        It is a {color} {model} from {year}.
+      </p>
+    </>
   )
 }
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FavoriteColor />);
+root.render(<Car />);
