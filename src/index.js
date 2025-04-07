83ed27cf6af51bf3bec0104faae7b82bb8a5d164
David J. Straley <djstraley@sandyhook.com>
2025-04-07 14:38:58 -0500

    using the updater function of useState() - demo

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 function FavoriteColor() {
-  const [color, setColor] = useState("blue");
+  const [color, setColor] = useState("red");
 
-  return <h1>My favorite color is {color}!</h1>
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+          >Blue</button>
+          
+          <button type="button" onClick={() => setColor("green")}>
+            Green
+          </button>
+      <button type="button" onClick={() => setColor("red")}>
+        Red
+      </button>
+      <button type="button" onClick={() => setColor("yellow")}>
+        Yellow
+          </button>
+      </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
