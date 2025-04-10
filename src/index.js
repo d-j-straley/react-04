89d9db1d0eb068639bed4a5451bba0285be1d7d3
David J. Straley <djstraley@sandyhook.com>
2025-04-09 20:39:52 -0500

    this demo shows how to set up the Ceanup function for the useEffect() function.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,52 @@
 import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-/* 
- * This example demonstrates how to use the useEffect hook to update a state variable
- * based on the value of another state variable.  
- * The useEffect hook is called whenever the count state variable changes,
- * and it updates the calculation state variable to be twice the value of count.
- * The calculation state variable is initialized to 0.
- * The useEffect hook is used to perform side effects in functional components.
- * In this case, the side effect is updating the calculation state variable
- * whenever the count state variable changes.
- * The useEffect hook takes two arguments: a function to run and an array of dependencies.
- * The function will run whenever any of the dependencies change.
- * In this case, the function updates the calculation state variable
- */
-
-function Counter() {
+function Timer() {
   const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
 
+  // useEffect is called after the component is rendered
+  // and after every update
+
+  // useEffect takes a function as an argument
+  // this function is called after the component is rendered
+  // and after every update
+  // useEffect can return a function
+  // this function is called when the component is unmounted
+  // and before the next effect is run
+  // this is useful for cleaning up resources
+  // such as timers, subscriptions, etc.
+  // useEffect can also take an array of dependencies
+  // this array tells React when to run the effect
+  // if the array is empty, the effect is only run once
+  // if the array is not empty, the effect is run when
+  // any of the dependencies change
+  // in this case, the effect is run every time the count changes
+  // if the effect returns a function, that function is called
+  // when the component is unmounted or before the next effect is run
+  // this is useful for cleaning up resources such as timers, subscriptions, etc.
+  // useEffect(() => {
+  //   console.log('Component mounted');
+  //   return () => {
+  //     console.log('Component unmounted');
+  //   };
+  // }, []);  
+  // 
   useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]); // <- add the count variable here
+    let timer = setTimeout(() => {
+    setCount((count) => count + 1);
+  }, 1000);
 
-  return (
-    <>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
-      <p>Calculation: {calculation}</p>
-    </>
-  );
+    // the following is the cleanup function.
+    // it has a function similar to a C++ destructor.
+  return () => clearTimeout(timer)
+  }, []);
+
+  return <h1>I've rendered {count} times!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter />);
+root.render(<Timer />);
+
+/*
+Note: To clear the timer, we had to name it.
+*/
