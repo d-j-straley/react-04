c595d47a86caf0e7eab4830e006245441e5f9e69
David J. Straley <djstraley@sandyhook.com>
2025-04-10 12:16:06 -0500

    demonstration of passing the local useState from component to component

--- src/index.js
+++ src/index.js
@@ -1,52 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-function Timer() {
-  const [count, setCount] = useState(0);
+function Component1() {
+  const [user, setUser] = useState("D.J. Straley");
 
-  // useEffect is called after the component is rendered
-  // and after every update
+  return (
+    <>
+      <h1>{`Hello ${user}!`}</h1>
+            <h2>{`Hello ${user} #1!`}</h2>     <Component2 user={user} />
+    </>
+  );
+}
 
-  // useEffect takes a function as an argument
-  // this function is called after the component is rendered
-  // and after every update
-  // useEffect can return a function
-  // this function is called when the component is unmounted
-  // and before the next effect is run
-  // this is useful for cleaning up resources
-  // such as timers, subscriptions, etc.
-  // useEffect can also take an array of dependencies
-  // this array tells React when to run the effect
-  // if the array is empty, the effect is only run once
-  // if the array is not empty, the effect is run when
-  // any of the dependencies change
-  // in this case, the effect is run every time the count changes
-  // if the effect returns a function, that function is called
-  // when the component is unmounted or before the next effect is run
-  // this is useful for cleaning up resources such as timers, subscriptions, etc.
-  // useEffect(() => {
-  //   console.log('Component mounted');
-  //   return () => {
-  //     console.log('Component unmounted');
-  //   };
-  // }, []);  
-  // 
-  useEffect(() => {
-    let timer = setTimeout(() => {
-    setCount((count) => count + 1);
-  }, 1000);
+function Component2({ user }) {
+  return (
+    <>
+      <h1>Component 2</h1>
+            <h2>{`Hello ${user} #2!`}</h2>     <Component3 user={user} />
+    </>
+  );
+}
 
-    // the following is the cleanup function.
-    // it has a function similar to a C++ destructor.
-  return () => clearTimeout(timer)
-  }, []);
+function Component3({ user }) {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <h2>{`Hello ${user} #3!`}</h2>           <Component4 user={user} />
+    </>
+  );
+}
 
-  return <h1>I've rendered {count} times!</h1>;
+function Component4({ user }) {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <h2>{`Hello ${user} #4!`}</h2>      <Component5 user={user} />
+
+    </>
+  );
+}
+
+function Component5({ user }) {
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} #5 again!`}</h2>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Timer />);
-
-/*
-Note: To clear the timer, we had to name it.
-*/
+root.render(<Component1 />);
