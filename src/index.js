b5bd0f2db6c521a991197e3c3628d8de64c14371
David J. Straley <djstraley@sandyhook.com>
2025-04-12 13:21:29 -0500

    another demo showing useRef ( ) usage

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-import { useState, useEffect, useRef } from "react";
+
+import { useRef } from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const count = useRef(0);
+  const inputElement = useRef();
 
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
+  const focusInput = () => {
+    inputElement.current.focus();
+  };
 
   return (
     <>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <h1>Render Count: {count.current}</h1>
+      <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button>
     </>
   );
 }
@@ -25,6 +21,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 /*
-Try typing in the input field, and you will
-see the application render count increase.
+By clicking the button,
+the input field will get focus.
 */
