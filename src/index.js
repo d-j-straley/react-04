bf26aa93238cc4ec7f68b8a0df800ce668c96208
David J. Straley <djstraley@sandyhook.com>
2025-04-11 22:31:54 -0500

    demo of useRef ( )

--- src/index.js
+++ src/index.js
@@ -1,53 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 
-function Component1() {
-  const [user, setUser] = useState("D.J. Straley");
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
 
   return (
     <>
-      <h1>{`Hello ${user}!`}</h1>
-            <h2>{`Hello ${user} #1!`}</h2>     <Component2 user={user} />
-    </>
-  );
-}
-
-function Component2({ user }) {
-  return (
-    <>
-      <h1>Component 2</h1>
-            <h2>{`Hello ${user} #2!`}</h2>     <Component3 user={user} />
-    </>
-  );
-}
-
-function Component3({ user }) {
-  return (
-    <>
-      <h1>Component 3</h1>
-      <h2>{`Hello ${user} #3!`}</h2>           <Component4 user={user} />
-    </>
-  );
-}
-
-function Component4({ user }) {
-  return (
-    <>
-      <h1>Component 4</h1>
-      <h2>{`Hello ${user} #4!`}</h2>      <Component5 user={user} />
-
-    </>
-  );
-}
-
-function Component5({ user }) {
-  return (
-    <>
-      <h1>Component 5</h1>
-      <h2>{`Hello ${user} #5 again!`}</h2>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
     </>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Component1 />);
+root.render(<App />);
+
+/*
+Try typing in the input field, and you will
+see the application render count increase.
+*/
