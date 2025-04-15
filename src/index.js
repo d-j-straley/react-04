6ce8c78c26d77e042782de03ce838a58fdcf77d0
David J. Straley <djstraley@sandyhook.com>
2025-04-15 13:08:35 -0500

    demonstration of web hook "useMemo"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
-import Todos from "./Todos";
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
 
   const increment = () => {
     setCount((c) => c + 1);
@@ -14,16 +14,29 @@ const App = () => {
   };
 
   return (
-    <>
-      <Todos todos={todos} addTodo={addTodo} />
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
       <hr />
       <div>
         Count: {count}
         <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
       </div>
-    </>
+    </div>
   );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
