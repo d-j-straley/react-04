d41797a0751c86b0cda21134bc7d57b9951ec605
David J. Straley <djstraley@sandyhook.com>
2025-04-15 13:13:03 -0500

    lowered the calculation loop

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ const App = () => {
 
 const expensiveCalculation = (num) => {
   console.log("Calculating...");
-  for (let i = 0; i < 1000000000; i++) {
+  for (let i = 0; i < 10; i++) {
     num += 1;
   }
   return num;
